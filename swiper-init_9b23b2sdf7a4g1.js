64800a660bd000c2e79b0e27665623d91d71c4d2
dep <96613150+depinsol@users.noreply.github.com>
2025-06-17 14:51:10 +0300

    Update swiper-init_9b.js

--- swiper-init_9b23b2sdf7a4g1.js
+++ swiper-init_9b23b2sdf7a4g1.js
@@ -1,18 +1,4 @@
-    const swiperPartners = new Swiper('.swiper-partners', {
-            slidesPerView: 2,
-            spaceBetween: 16,
-            loop: true,
-            grabCursor: true,
-            autoplay: {
-                delay: 1500,
-                disableOnInteraction: false,
-            },
-            breakpoints: {
-                640: { slidesPerView: 2 },
-                768: { slidesPerView: 4 },
-                1024: { slidesPerView: 6 },
-            },
-        });
+
 
         const swiperClients = new Swiper('.swiper-clients', {
      slidesPerView: 2,
